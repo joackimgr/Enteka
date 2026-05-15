f07346b84e46757af99408827609e450911e52b6
Joackim Pantelakis <joackim.gr@gmail.com>
2026-05-15 16:14:49 +0300

    fix: set the account settings div width to full

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ export default function Sidebar(props) {
             }
             {!props.chatMode && 
                 <div className="bg-[#272B3D] rounded-[1.2rem] p-2.75 flex flex-col items-center gap-2.75">
-                    <div onClick={props.toggleStatus} className="bg-[#40465d] mb-2.75 p-2.75 box-border rounded-[1.2rem] flex items-center gap-5 cursor-pointer hover:bg-[#3a3f54] transition-colors duration-100 ease-in">
+                    <div onClick={props.toggleStatus} className="bg-[#40465d] mb-2.75 p-2.75 w-full box-border rounded-[1.2rem] flex items-center gap-5 cursor-pointer hover:bg-[#3a3f54] transition-colors duration-100 ease-in">
                         <img src={profile} alt="Account Settings Icon" className="w-12.5 h-12.5"/>
                         <p className="text-white text-[23px] font-light m-0">Account Settings</p>
                     </div>
