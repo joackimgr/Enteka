82c3da7e883af8c1a93b29facc2b8b63609b77f9
Joackim Pantelakis <joackim.gr@gmail.com>
2026-06-10 23:46:32 +0300

    fix: change input caret size

--- src/components/chat/NewMessage.jsx
+++ src/components/chat/NewMessage.jsx
@@ -38,7 +38,7 @@ export default function NewMessage() {
         <div className="bg-[#272B3D] flex flex-col items-center justify-start rounded-4xl p-2.75 text-white">
             <div className="flex items-center w-full bg-[#2F3347] h-auto p-2.75 text-4xl font-light box-border rounded-[1.2rem] gap-2.75 mb-2.75">
                 <label htmlFor="recipientName">To:</label>
-                <input type="text" id="recipientName" onChange={handleSearchText} className="w-full h-full box-border rounded-[1.2rem] border-0 bg-[#2F3347] text-[30px] text-white focus:outline-none"/>
+                <input type="text" id="recipientName" onChange={handleSearchText} className="w-full py-2 box-border rounded-[1.2rem] border-0 bg-[#2F3347] text-[30px] text-white focus:outline-none"/>
             </div>
             <div className="flex flex-col p-2.75 w-full bg-[#2F3347] box-border rounded-[1.2rem]">
                 <p className="text-2xl mt-1.25 mb-3.75">Suggestions:</p>
