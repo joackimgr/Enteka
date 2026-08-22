ee299baf05e75f4c72829e6e3c5db4133416b355
Joackim Pantelakis <joackim.gr@gmail.com>
2026-08-22 23:43:54 +0300

    refactor: add type hints to security/auth.py

--- Backend/security/auth.py
+++ Backend/security/auth.py
@@ -1,24 +1,25 @@
 from fastapi import HTTPException, status
 import jwt
 import os
+from typing import Any
 from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
 
 load_dotenv()
 
-key = os.getenv("SECRET_KEY")
-algorithm = os.getenv("ALGORITHM")
-token_expire = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+key: str | None = os.getenv("SECRET_KEY")
+algorithm: str | None = os.getenv("ALGORITHM")
+token_expire: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 if not key:
     raise RuntimeError("SECRET_KEY environment variable is missing!")
 
-def create_access_token(username):
+def create_access_token(username: str) -> str:
     exp = (datetime.now(timezone.utc) + timedelta(minutes=token_expire)).timestamp()
     token = jwt.encode({"sub":username, "exp":exp}, key, algorithm)
     return token
 
-def verify_token(token):
+def verify_token(token: str) -> dict[str, Any]:
     try:
         payload = jwt.decode(token, key, algorithms=[algorithm])
         username = payload.get("sub")
